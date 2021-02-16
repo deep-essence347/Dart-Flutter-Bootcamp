b81aeb548b346c393cad05af7414d4fc2cf0cb7f
Dipesh Shrestha <51238569+deep-essence347@users.noreply.github.com>
2021-02-16 12:22:50 +0545

    abcd

--- Flutter/xylophone/lib/main.dart
+++ Flutter/xylophone/lib/main.dart
@@ -4,22 +4,23 @@ import 'package:audioplayers/audio_cache.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
-
-  void playSound(int audioIndex){
+  void playSound(int audioIndex) {
     final player = AudioCache();
     player.play('note$audioIndex.wav');
   }
 
-  Expanded buildKey({Color color,int audioIndex}){
+  Expanded buildKey({Color color, int audioIndex}) {
     return Expanded(
       child: FlatButton(
         color: color,
-        onPressed: (){
+        child: null,
+        onPressed: () {
           playSound(audioIndex);
         },
       ),
     );
   }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -29,13 +30,13 @@ class XylophoneApp extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              buildKey(color: Colors.red,audioIndex: 1),
-              buildKey(color: Colors.orange,audioIndex: 2),
-              buildKey(color: Colors.yellow,audioIndex: 3),
-              buildKey(color: Colors.green,audioIndex: 4),
-              buildKey(color: Colors.blue,audioIndex: 5),
-              buildKey(color: Colors.teal,audioIndex: 6),
-              buildKey(color: Colors.purple,audioIndex: 7),
+              buildKey(color: Colors.red, audioIndex: 1),
+              buildKey(color: Colors.orange, audioIndex: 2),
+              buildKey(color: Colors.yellow, audioIndex: 3),
+              buildKey(color: Colors.green, audioIndex: 4),
+              buildKey(color: Colors.blue, audioIndex: 5),
+              buildKey(color: Colors.teal, audioIndex: 6),
+              buildKey(color: Colors.purple, audioIndex: 7),
             ],
           ),
         ),
